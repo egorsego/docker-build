0b5ac322075406e6b6f61fcccc5396403229bac6
Egor Alexandrov <egor4ik@gmail.com>
2019-12-06 18:41:41 +0300

    jenkinsfile updated

--- ci/bash_scripts/stage_start.sh
+++ ci/bash_scripts/stage_start.sh
@@ -6,4 +6,4 @@ CONTEXT=$2
 curl "https://api.GitHub.com/repos/egorsego/docker-build/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"pending\", \"context\": \"$CONTEXT\", \"target_url\": \"http://192.168.242.43:8080/job/pr_test/$BUILD_NUMBER\"}"
+-d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: $CONTEXT Stage\", \"target_url\": \"http://192.168.242.43:8080/job/pr_test/$BUILD_NUMBER\"}"
--- ci/bash_scripts/stage_success.sh
+++ ci/bash_scripts/stage_success.sh
@@ -6,4 +6,4 @@ CONTEXT=$2
 curl "https://api.GitHub.com/repos/egorsego/docker-build/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"success\", \"context\": \"$CONTEXT\", \"description\": \"Completed Successfully\", \"target_url\": \"http://192.168.242.43:8080/job/pr_test/$BUILD_NUMBER\"}"
+-d "{\"state\": \"success\", \"context\": \"Jenkins-CI: $CONTEXT Stage\", \"description\": \"Completed Successfully\", \"target_url\": \"http://192.168.242.43:8080/job/pr_test/$BUILD_NUMBER\"}"
