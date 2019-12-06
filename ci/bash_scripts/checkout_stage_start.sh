ecef1b8f4abbb1810991181ce6fded5183df8163
Egor Alexandrov <egor4ik@gmail.com>
2019-12-06 14:42:37 +0300

    script updated

--- ci/bash_scripts/checkout_stage_start.sh
+++ ci/bash_scripts/checkout_stage_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://api.GitHub.com/repos/egorsego/docker-build/statuses/$GIT_COMMIT?access_token=3bf32d587b844411493edee30f31122fd6c1ded6" \
+curl "https://api.GitHub.com/repos/egorsego/docker-build/statuses/$GIT_COMMIT?access_token=b350185424351d434b105ff242d79de170fcc8e8 " \
 -H "Content-Type: application/json" \
 -X POST \
 -d "{\"state\": \"pending\", \"description\": \"Jenkins-CI: Checkout Stage Started\", \"target_url\": \"192.168.242.43/job/pr_test/${env.BUILD_NUMBER}/console\"}"
